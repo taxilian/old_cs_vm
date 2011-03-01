8089ecfe2254a98e9da7952b668fda63ed8f3dd0
Richard Bateman <taxilian@gmail.com>
2011-03-01 15:52:49 -0700

    Misc tweaks, renames, namespace, etc

--- OperatingSystem/FileSystem.h
+++ OperatingSystem/FileSystem.h
@@ -1,41 +1,50 @@
+#pragma once
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
-const size_t fileDataBlks = 20;//every block is 512 bytes which gives a maximum of 10KB file size.
-const size_t lenghtPath = 60;
-const size_t blkSize = 512;
-const size_t numOfEntries = 10;//maximum number of directory or file entries in a directory.
-enum iNType{directory,file,link};
-struct Inode{
-	size_t creationDate;//YYYYMMDD
-	size_t lastChDate;//YYYYMMDD
-};
-struct iNFile: Inode{
-	size_t fileSize;
-	size_t dataBLKS[fileDataBlks];
-};
-struct iNLink: Inode{
-	char pathName[lenghtPath];
-};
-struct Data{
-	char rawData[blkSize]; 
-	Data(){
-		for(int i = 0; i < blkSize; i++)
-		{rawData[i] = '3';}//initialize bytes of data with some char.
-	}
-};
-struct Entry{
-	iNType type;
-	char name[60];
-	Inode* iNptr;
-};
-struct Directory{
-	size_t size;//number of entries in directory.
-	Inode* next;
-	Entry entries[numOfEntries];//if entries exceed 10 then Inode points to next dir block.
-};
-class FileSystem
-{
 
-};
+#include "VMCore.h"
+#include "VirtualDisk.h"
 
+namespace OS {
+
+    const size_t fileDataBlks = 20;//every block is 512 bytes which gives a maximum of 10KB file size.
+    const size_t lengthPath = 80;// 20 * 4 is 80 bytes, our paths should also be 80 bytes
+    const size_t blkSize = VM::VirtualDisk::BLOCK_SIZE;
+    const size_t numOfEntries = 10;//maximum number of directory or file entries in a directory.
+
+    enum iNType{directory,file,link};
+    struct iNFile {
+        uint64_t creationDate;//YYYYMMDD
+        uint64_t modifyDate;//YYYYMMDD
+        uint32_t fileSize;
+        uint32_t dataBLKS[fileDataBlks];
+    };
+    struct iNLink {
+        uint64_t creationDate;//YYYYMMDD
+        uint64_t modifyDate;//YYYYMMDD
+        uint32_t fileSize;
+        char pathName[lengthPath];
+    };
+    struct Data{
+        char rawData[blkSize]; 
+        Data(){
+            memset(rawData, 0xF0, blkSize);
+        }
+    };
+    struct Entry{
+        iNType type;
+        char name[lengthPath];
+        uint32_t iNode;
+    };
+    struct Directory{
+        size_t size;//number of entries in directory.
+        uint32_t next;
+        Entry entries[numOfEntries];//if entries exceed 10 then Inode points to next dir block.
+    };
+    class FileSystem
+    {
+
+    };
+
+}
 #endif
--- vm_main/VirtualDisk.h
+++ vm_main/VirtualDisk.h
@@ -4,19 +4,22 @@
 #include <iostream>
 #include <fstream>
 
-class VirtualDisk
-{
-public:
-    const static int BLOCK_SIZE = 512;
-public:
-    VirtualDisk(const std::string& fileName);
-    ~VirtualDisk(void);
+namespace VM {
 
-    void ReadFromDisk(int block, size_t len, char* retPtr);
-    void WriteToDisk(int block, size_t len, char* data);
-    void Erase(int blockCount = 2048);
+    class VirtualDisk
+    {
+    public:
+        const static int BLOCK_SIZE = 512;
+    public:
+        VirtualDisk(const std::string& fileName);
+        ~VirtualDisk(void);
 
-private:
-    std::string m_filename;
+        void ReadFromDisk(int block, size_t len, char* retPtr);
+        void WriteToDisk(int block, size_t len, char* data);
+        void Erase(int blockCount = 2048);
 
+    private:
+        std::string m_filename;
+
+    };
 };
